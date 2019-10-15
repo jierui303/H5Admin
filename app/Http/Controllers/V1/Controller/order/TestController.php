6ebd97d14c49d7cf7bae55bbbf273c8ec01511e6
王瑞杰 <jierui303@163.com>
2019-10-15 16:37:19 +0800

    增加apidoc示例

--- app/Http/Controllers/V1/Controller/order/TestController.php
+++ app/Http/Controllers/V1/Controller/order/TestController.php
@@ -11,6 +11,29 @@ use App\Http\Models\OrderModel;
 use Illuminate\Http\Request;
 use Wrj\Order;
 
+/**
+ * @api {POST} http://aaa.com/index/Index/add_needs 测试接口文档 APIDoc示例
+ * @apiVersion 1.0.0
+ * @apiGroup NEED
+ *
+ * @apiParam {String} need_name 需求者名称-非空
+ * @apiParam {String} e_mail 用户邮箱-非空邮箱格式
+ * @apiParam  {String} phone 用户电话-非空
+ * @apiParam {String} company_name 需求公司名称-非空
+ * @apiParam  {String} needs_desc 需求描述-非空
+ *
+ * @apiSuccess {Object} code 返回码
+ * @apiSuccess {Object} reason  中文解释
+ * @apiSuccess {String[]} data  返回数据
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "code":0,
+ *          "reason":"需求已经提交了，我们的工作人员会在2个工作日内和您取得联系!",
+ *          "data":[]
+ *      }
+ */
 class TestController extends CommanController
 {
 
